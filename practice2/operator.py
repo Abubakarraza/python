faee3a6c16d577728b76383cf7c00c7aa9a5fc5a
M Abubakar Raza <abubakarraza64@gmail.com>
2023-02-13 23:16:23 +0500

    dataType and Opreator

---
+++ practice2/operator.py
@@ -0,0 +1,24 @@
+# python has different operator used to solve different mathematics equation
+# addition + 
+add = 34 + 34
+# subtraction - 
+sub = 45 - 90
+# multiply * 
+multiply = 56 * 78
+# Exponential ** 
+expo = 89**4
+# Division 
+division = 78 / 2
+# Floor Division //
+# it does not give you point value
+floorDivision = 79//2
+# Modulus % 
+# it just give you reminder 
+modulus = 89 /2
+print("add : ",add)
+print("sub : ",sub)
+print("multiply : ",multiply)
+print("expo : ",expo)
+print("division : ",division)
+print("floorDivision :",floorDivision )
+print("modulus : ",modulus)
