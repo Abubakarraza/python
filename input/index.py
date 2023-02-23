4c3a06480421d3e32d1c788f86aa5b07c260a499
M Abubakar Raza <abubakarraza64@gmail.com>
2023-02-23 21:02:33 +0500

    input in python

---
+++ input/index.py
@@ -0,0 +1,8 @@
+# You can take input from user using input() function it wait for user to type 
+# Enter Your Name which i used below work as label of input 
+# input only take string value then we convert value into desired DataType 
+a = input("Enter Your Name:")
+print("My Name is",a)
+num =input("Enter first Number:")
+num2 = input("Enter Second Number:")
+print("Result:",int(num) + int(num2))
