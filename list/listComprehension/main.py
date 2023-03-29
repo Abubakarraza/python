1723ed7e1d9138fea36cf87e91d6a2d1f8692085
M Abubakar Raza <abubakarraza64@gmail.com>
2023-03-29 23:16:48 +0500

    comprehension in list

--- list/listComprehension/main.py
+++ list/listComprehension/main.py
@@ -1,7 +1,22 @@
-# List comprehension is used for creating new list from other iterable like list,tuple,dictionary,sets and even in array and string
+# List comprehension is used for creating new list from other iterable like list,tuple,dictionary,sets , even in array and string
 
 
 # Syntax
 # List = [Expression(item) for item in iterable if condition]
 
-# Expression : 
+# Expression : It is the item which is being iterated
+
+# Iterable : it can be list tuple ,sets, dictionary ,even in array and string
+
+# Condition : condition check if the item should be added to the new list or not
+
+# Example# 1 :
+
+lst=[i for i in range(5)]
+print(lst)
+
+
+# Example# 2 :
+lst1=[i*i for i in range(20) if i % 2 == 0]
+# it will all  print even number
+print(lst1)
