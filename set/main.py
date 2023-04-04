019ad46de6ac59a51f4d17975acdcae2aee20f27
M Abubakar Raza <abubakarraza64@gmail.com>
2023-04-04 22:36:59 +0500

    set in python

---
+++ set/main.py
@@ -0,0 +1,22 @@
+# Set in Python 
+
+# Sets are unordered collection of data items. They store multiple items in a single variable . set items are separated by commas and enclosed within curly brackets{}. Sets are unchangeable , meaning you cannot change items of the set once created . Sets do not contain duplicate item
+
+# Example 
+
+info={"Abubakar", 90, False, 7.7,90}
+print(info)
+# you can check 90 print once 👆 not twice because set do not contain duplicate items
+
+# Here we see that the items of set occur in random order and hence they cannot be accessed using index numbers. Also sets do not allow duplicate values.
+
+
+st = {}
+print(type(st))
+# The output will be the dic😮😨
+# because dict is also initialize with {}
+# so if we want to initialize empty set we use set() instead of {}
+
+st2=set()
+print(type(st2))
+# Now it will be set 😍😎
