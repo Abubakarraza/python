af8d8aaaaa1e46bf3a407b6821c1929a0c87945c
M Abubakar Raza <abubakarraza64@gmail.com>
2023-04-04 22:15:34 +0500

    fibonacci sequence in python

--- recursion/main.py
+++ recursion/main.py
@@ -14,6 +14,20 @@ def factorial (n):
         return 1
     else:
         return n * factorial(n-1)
-num=7
+num =int( input("Enter any number:"))
 print("number:",num)
 print("Factorial of ",num,"is",factorial(num))
+
+
+
+# fibonacci-sequence
+
+def fibonacci(n):
+    if(n==0):
+        return 0
+    elif(n==1):
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+    
+print("fibonacci:",fibonacci(3))    
