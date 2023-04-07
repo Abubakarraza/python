7fd07a9f271a417370d688c2d5d68102a6efb0f0
M Abubakar Raza <abubakarraza64@gmail.com>
2023-04-08 00:03:43 +0500

    finally keyword with try and except

--- exceptionHandling/main.py
+++ exceptionHandling/main.py
@@ -45,3 +45,30 @@ except Exception as e:
 
 
 # There are many type error handler in python we discuss it later
+
+
+# Finally Clause 
+
+# The finally code block is also a part of exception handling. When we handle exception using the try and except block , we can include a finally block at the end. The finally block is always executed, so it generally used for doing the concluding tasks like closing file resources or closing database connection or may be ending the program execution with a delightful message
+
+
+
+# Syntax 
+
+# try :
+    #    statement which could generate exception
+# except: 
+    #  solution of generated exception
+# finally: 
+         # block of code which is going execute in any case 
+
+
+# Example :
+try:
+    num = int(input("Enter an integer: "))
+except ValueError:
+    print("Number entered is not an integer.")
+else:
+    print("Integer Accepted.")
+finally:
+    print("This block is always executed.")
