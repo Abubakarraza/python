aa61a6a86909cb63334a09c1cedab5694b8f4e4b
M Abubakar Raza <abubakarraza64@gmail.com>
2023-04-12 23:12:59 +0500

    short hand  if else

---
+++ shortHandIfElse/main.py
@@ -0,0 +1,8 @@
+# Short Hand If-else
+
+# There is also a shorthand syntax for the if-else statement that can be used when the condition being tested is simple and the code blocks to be executed are short. Here's an example:
+
+a = 2 
+b = 40
+
+print("A") if a > b else print("=") if a==b else print("B")
