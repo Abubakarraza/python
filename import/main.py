a8b291d2e0830666965142da960a20706463e08e
Abubakarraza <abubakarraza64@gmail.com>
2023-04-28 11:52:42 -0700

    how import work

---
+++ import/main.py
@@ -0,0 +1,8 @@
+
+# Importing in Python is the process of loading code from a Python module into the current script. This allows you to use the functions and variables defined in the module in your current script, as well as any additional modules that the imported module may depend on.
+
+# To import a module in Python, you use the import statement followed by the name of the module. For example, to import the math module, which contains a variety of mathematical functions, you would use the following statement:
+import math
+import raza
+print("multiply:",raza.multiply(2,4))
+print("sqrt:",math.sqrt(9))
